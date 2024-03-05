af5372282aa7a926c2dbbec758ba87ae9ee34488
smmstakes <matheusds014@gmail.com>
2024-03-04 22:50:11 -0300

    QUIZZ_APP #06: adding some styling features

--- PROJECTS/quizz_app/lib/widgets/questions_summary.dart
+++ PROJECTS/quizz_app/lib/widgets/questions_summary.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsSummary extends StatelessWidget {
   const QuestionsSummary({super.key, required this.sumarryData});
@@ -8,23 +8,57 @@ class QuestionsSummary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(height:300,
+    Color? correctColors(Map<String, Object> data) {
+      return data["user_answer"] == data["correct_answer"]
+          ? const Color.fromARGB(255, 3, 179, 38)
+          : const Color.fromARGB(255, 255, 38, 0);
+    }
+
+    return SizedBox(
+      height: 300,
       child: SingleChildScrollView(
         child: Column(
           children: sumarryData.map(
             (data) {
+              final Color? correctColor = correctColors(data);
               return Row(
                 children: [
-                  Text(((data['questions_id'] as int) + 1).toString(),),
+                  CircleAvatar(
+                      radius: 30,
+                      backgroundColor: correctColor,
+                      child: Text(
+                        style: GoogleFonts.lato(
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
+                        ((data['questions_id'] as int) + 1).toString(),
+                      )),
                   Expanded(
                     child: Column(
                       children: [
-                        Text(data["question"] as String),
+                        Text(
+                            softWrap: true,
+                            textAlign: TextAlign.center,
+                            style: GoogleFonts.lato(
+                              color: Colors.white,
+                              fontSize: 17,
+                            ),
+                            data["question"] as String),
                         const SizedBox(
                           height: 5,
                         ),
-                        Text(data["user_answer"] as String),
-                        Text(data["correct_answer"] as String),
+                        Text(
+                            style: GoogleFonts.lato(
+                              color: correctColor,
+                              fontSize: 16,
+                            ),
+                            data["user_answer"] as String),
+                        Text(
+                            style: GoogleFonts.lato(
+                              color: const Color.fromARGB(255, 3, 179, 38),
+                              fontSize: 16,
+                            ),
+                            data["correct_answer"] as String),
                       ],
                     ),
                   ),
