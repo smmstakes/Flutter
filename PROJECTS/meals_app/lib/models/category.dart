c59dca8a45624bdf6ed09e6671743b7dbe74048d
smmstakes <matheusds014@gmail.com>
2024-03-13 16:17:22 -0300

    MEALS_APP #01: building the app core

---
+++ PROJECTS/meals_app/lib/models/category.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Category {
+  const Category({required this.id, required this.color, required this.title});
+
+  final String id;
+  final Color color;
+  final String title;
+}
