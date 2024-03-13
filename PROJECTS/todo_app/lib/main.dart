3b3f31df4e3d6826c9ae67018be2d9449e0652ad
smmstakes <matheusds014@gmail.com>
2024-03-13 15:18:53 -0300

    TODO_APP #02: finishing the app

--- PROJECTS/todo_app/lib/main.dart
+++ PROJECTS/todo_app/lib/main.dart
@@ -1,6 +1,26 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:todo_app/keys/keys.dart';
+
+// import 'package:todo_app/ui_updates_demo.dart';
 
 void main() {
-  // runApp();
+  runApp(const App());
 }
 
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Internals'),
+        ),
+        body: const Keys(),
+      ),
+    );
+  }
+}
